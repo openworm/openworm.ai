bb5966fc1acfe0940a0ef847767c2aebffc10a6e
pgleeson <p.gleeson@gmail.com>
2024-12-17 10:01:49 +0000

    Initial ideas for quiz

---
+++ quiz/Templates.py
@@ -0,0 +1,13 @@
+'''
+Generate a list of 5 multiple choice questions to test someone's general knowledge. 
+The questions should be answerable by a reasonably intelligent school child. 
+There should be 4 possible answers, only one of which is correct, and all of the answers should be kept brief.
+Each of the 5 questions should be presented in the following format:
+
+QUESTION: What is the capital of France?
+CORRECT ANSWER: Paris
+WRONG ANSWER: Madrid
+WRONG ANSWER: Rome
+WRONG ANSWER: Dublin
+
+'''
