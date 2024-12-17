7e349ab960cd2ac56f1a2152182a3a0f099dca7e
pgleeson <p.gleeson@gmail.com>
2024-12-17 14:23:40 +0000

    Improved tests

--- regenerateAndTest.sh
+++ regenerateAndTest.sh
@@ -10,7 +10,16 @@ python -m openworm_ai.quiz.QuizModel
 
 python -m openworm_ai.parser.ParseWormAtlas
 
+
+if [ $# -eq 1 ] ; then
+    if [ $1 == "-free" ]; then
+        python -m openworm_ai.graphrag.GraphRAG_test -test
+    fi
+else 
+        
 python -m openworm_ai.graphrag.GraphRAG_test
+
+fi
 
 
 
