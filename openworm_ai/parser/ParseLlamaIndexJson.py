a4b4bfcd159fa0b49cbc8f8d4fb4688c0391fa04
pgleeson <p.gleeson@gmail.com>
2025-01-15 16:23:59 +0000

    Placeholder file

---
+++ openworm_ai/parser/ParseLlamaIndexJson.py
@@ -0,0 +1,16 @@
+
+
+from openworm_ai.parser.DocumentModels import Document, Section, Paragraph
+
+MARKDOWN_DIR = "processed/markdown/wormatlas"
+PLAINTEXT_DIR = "processed/plaintext/wormatlas"
+JSON_DIR = "processed/json/wormatlas"
+
+CORPUS_LOCATION = "openworm_ai/corpus"
+
+
+
+
+
+if __name__ == "__main__":
+    papers = {}
