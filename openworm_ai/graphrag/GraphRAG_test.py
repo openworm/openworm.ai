e9a7ed341b27ba496c94d29de4cf92cb7901490d
pgleeson <p.gleeson@gmail.com>
2025-02-28 11:21:21 +0000

    Add Corsi et al 2015

--- openworm_ai/graphrag/GraphRAG_test.py
+++ openworm_ai/graphrag/GraphRAG_test.py
@@ -98,7 +98,8 @@ SOURCES:
         "What is the main function of cell AVBR?",
         "Give me 3 facts about the coelomocyte system in C. elegens",
         "Give me 3 facts about the control of motor programs in c. elegans by monoamines",
-        "The NeuroPAL transgene is cool. Give me some examples of fluorophores in it.",
+        "The NeuroPAL transgene is amazing. Give me some examples of fluorophores in it.",
+        "When was the first metazoan genome sequenced? Answer only with the year.",
     ]
 
     for query in queries:
