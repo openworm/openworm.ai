20a86f7adc3c9a1ea0410406ff2d9baff2cfde70
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2025-11-12 16:23:46 +0000

    fix(langchain): update import
    Should fix CI

--- openworm_ai/utils/llms.py
+++ openworm_ai/utils/llms.py
@@ -1,6 +1,6 @@
 import time
 
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain.chat_models import init_chat_model
 
